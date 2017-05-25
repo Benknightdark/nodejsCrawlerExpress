f25b7792fa4b65ec888338bd3a4b08a9743c7847
龔彥賓 <Ben@gongyanbindeMBP.mshome.net>
2017-05-25 17:52:20 +0800

    OK

--- index.js
+++ index.js
@@ -17,34 +17,39 @@ var app = express()
 app.use(cors())
 
 app.get('/', function (req, res, next) {
+    var data = []
+    rp('https://netbank.jihsunbank.com.tw/Rate/ExgRate.htm')
+        .then(function (htmlString) {
 
- rp('https://netbank.jihsunbank.com.tw/Rate/ExgRate.htm')
-    .then(function (htmlString) {
-      var $ = cheerio.load(htmlString);
+            var $ = cheerio.load(htmlString);
             for (var a = 0; a < $("td[class=TitleForRate]").length; a++) {
-               
+
                 if ($($("td[class=TitleForRate]")[a]).text() == "澳幣") {
                     var CurrencyType = $($("td[class=TitleForRate]")[a]).text()
                     var day = dateFormat(new Date().now, "yyyy-mm-dd h:MM:ss");
                     var time = $("em");
-                    console.log("實際更新時間：",day);
-                    console.log("最新更新時間：", $(time).text())                    
-                    console.log("幣別：",$($("td[class=TitleForRate]")[a]).text())
+                    console.log("實際更新時間：", day);
+                    console.log("最新更新時間：", $(time).text())
+                    console.log("幣別：", $($("td[class=TitleForRate]")[a]).text())
                     var BuyRate = $($("td[class=TitleForRate]")[a]).next()
-                    console.log("買匯：",$(BuyRate).text())
+                    console.log("買匯：", $(BuyRate).text())
                     var SaleRate = $(BuyRate).next()
-                    console.log("賣匯:",$(SaleRate).text())
+                    console.log("賣匯:", $(SaleRate).text())
                     console.log("=======================================");
+
+                    data.push([day, $(time).text(), $($("td[class=TitleForRate]")[a]).text(), $(BuyRate).text(), $(SaleRate).text()])
                 }
 
-                
-            }})
 
-  res.json({msg:"fff"})
+            }
+            res.send({
+                data
+            })
+        })
+
+
 })
 
 app.listen(8099, function () {
-  console.log('CORS-enabled web server listening on port 80')
+    console.log('CORS-enabled web server listening on port 80')
 })
-
-    
